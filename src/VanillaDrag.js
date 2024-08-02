b13aeb6e8e65ff3332dca4e6ef57f9ccae2fd1cf
GilTorch <infotorch2014@gmail.com>
2024-08-02 13:47:12 -0400

    Drag list

--- src/VanillaDrag.js
+++ src/VanillaDrag.js
@@ -3,7 +3,6 @@ import { peopleData } from "./data";
 /**  */
 const VanillaDrag = () => {
   const [peoples, setPeoples] = useState([...peopleData]);
-
   const [isItemIsDragging, setIsItemDragging] = useState(false);
   const dragPerson = useRef({ listPosition: 0, itemPosition: 0 });
   const draggedOverPerson = useRef({ listPosition: 0, itemPosition: 0 });
@@ -14,7 +13,7 @@ const VanillaDrag = () => {
     const peoplesClone = [...peoples];
     const temp = peoplesClone[dragPersonList.current];
     peoplesClone[dragPersonList.current] =
-      peoplesClone[draggedOverPerson.current];
+      peoplesClone[draggedOverPersonList.current];
     peoplesClone[draggedOverPersonList.current] = temp;
     setPeoples(peoplesClone);
   };
@@ -50,75 +49,72 @@ const VanillaDrag = () => {
   };
 
   return (
-    <main>
-      <div>
-        <h2>Vanilla Drag n Drop</h2>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          {peoples.map((people, peopleIndex) => (
-            <div
-              key={peopleIndex}
-              draggable={!isItemIsDragging}
-              onDragStart={() => {
-                dragPersonList.current = peopleIndex;
-              }}
-              onDragEnd={() => {
-                if (!isItemIsDragging) {
-                  onPersonListDragEnd();
-                }
-              }}
-              onDragEnter={() => {
-                draggedOverPersonList.current = peopleIndex;
-              }}
-              onDragOver={(e) => e.preventDefault()}
-              style={{
-                border: "1px solid black",
-                minHeight: "40vh",
-                minWidth: "10vw",
-              }}
-            >
-              {people.map((person, personIndex) => (
-                <div
-                  style={{
-                    width: "max-content",
-                    padding: "10px 5px",
-                    marginBottom: "10px",
-                    backgroundColor: "#eee",
-                  }}
-                  key={personIndex}
-                  draggable
-                  onDragStart={() => {
-                    setIsItemDragging(true);
-                    dragPerson.current = {
-                      listPosition: peopleIndex,
-                      itemPosition: personIndex,
-                    };
-                  }}
-                  onDragEnter={() => {
-                    draggedOverPerson.current = {
-                      listPosition: peopleIndex,
-                      itemPosition: personIndex,
-                    };
-                  }}
-                  onDragEnd={() => {
-                    setIsItemDragging(false);
-                    onPersonDragEnd();
-                  }}
-                  onDragOver={(e) => e.preventDefault()}
-                >
-                  <p>{person.name}</p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        {peoples.map((people, peopleIndex) => (
+          <div
+            key={peopleIndex}
+            draggable={!isItemIsDragging}
+            onDragStart={() => {
+              dragPersonList.current = peopleIndex;
+            }}
+            onDragEnd={() => {
+              if (!isItemIsDragging) {
+                onPersonListDragEnd();
+              }
+            }}
+            onDragEnter={() => {
+              draggedOverPersonList.current = peopleIndex;
+            }}
+            onDragOver={(e) => e.preventDefault()}
+            style={{
+              border: "1px solid black",
+              minHeight: "40vh",
+              minWidth: "10vw",
+            }}
+          >
+            {people.map((person, personIndex) => (
+              <div
+                style={{
+                  width: "max-content",
+                  padding: "10px 5px",
+                  marginBottom: "10px",
+                  backgroundColor: "#eee",
+                }}
+                key={personIndex}
+                draggable
+                onDragStart={() => {
+                  setIsItemDragging(true);
+                  dragPerson.current = {
+                    listPosition: peopleIndex,
+                    itemPosition: personIndex,
+                  };
+                }}
+                onDragEnter={() => {
+                  draggedOverPerson.current = {
+                    listPosition: peopleIndex,
+                    itemPosition: personIndex,
+                  };
+                }}
+                onDragEnd={() => {
+                  setIsItemDragging(false);
+                  onPersonDragEnd();
+                }}
+                onDragOver={(e) => e.preventDefault()}
+              >
+                <p>{person.name}</p>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    </main>
+    </div>
   );
 };
 
