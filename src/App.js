7f1f190d566234dca4b27476261e7bb1b734c755
GilTorch <infotorch2014@gmail.com>
2024-08-02 13:47:22 -0400

    Render multiple lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import VanillaDrag from './VanillaDrag';
+import React from "react";
+import VanillaDrag from "./VanillaDrag";
 
 const App = () => {
-
-    return <div>
-        <VanillaDrag />
+  return (
+    <div>
+      {new Array(3).fill(undefined).map((_, index) => (
+        <div key={index}>
+          <h2>List {index}</h2>
+          <VanillaDrag />
+        </div>
+      ))}
     </div>
-}
+  );
+};
 
 export default App;
